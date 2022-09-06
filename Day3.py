b956da296a80218e5b99fe02fced0721b15c947a
wasi_rehman09 <wasi786rehman@gmail.com>
2022-09-06 21:35:47 +0530

    Day3

---
+++ Day3.py
@@ -0,0 +1,19 @@
+#Day 3 ..Module Concept 
+# File can be imported using import filename method , variable is also imported with the import 
+# random module 
+import random
+
+#randomnumber
+randomNumber=random.randint(1,10)
+#generate number between 0,1 
+randomfloating=random.random() 
+print(randomNumber)
+print(randomfloating)
+
+
+#function in python is used by keyword def 
+
+def hello(name):
+    print("hello "+name)
+
+hello("wasi")
