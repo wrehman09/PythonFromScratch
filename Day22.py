f8f471ee6a96bfaa744b66be07ed91380c25c760
wasi_rehman09 <wasi786rehman@gmail.com>
2022-10-09 23:40:51 +0530

    Day22

---
+++ Day22.py
@@ -0,0 +1,14 @@
+#heart from turtle library  ;)
+
+from turtle import * 
+color('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
