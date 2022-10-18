283614c89c6291c1f2e90f164e363c3b99cc9e92
wasi_rehman09 <wasi786rehman@gmail.com>
2022-10-18 22:58:16 +0530

    Day31

--- Day31.py
+++ Day31.py
@@ -1,51 +1,49 @@
-#database sqllite and sqllitealchemy crud
-
-
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
+import sqlite3
 app = Flask(__name__)
 
-##CREATE DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
-#Optional: But it will silence the deprecation warning in the console.
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-##CREATE TABLE
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-
-
-db.create_all()
+all_books = []
+db = sqlite3.connect("books-collection.db", check_same_thread=False)
+cursor = db.cursor()
+#cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 
 @app.route('/')
 def home():
-    ##READ ALL RECORDS
-    all_books = db.session.query(Book).all()
-    return render_template("index.html", books=all_books)
+    global all_books
+    cursor.execute("SELECT * FROM books")
+
+    rows = cursor.fetchall()
+    print(rows)
+    #all_books = db.session.query(Book).all()
+    return render_template("index.html",all_books=all_books)
 
 
-@app.route("/add", methods=["GET", "POST"])
+@app.route("/add")
 def add():
-    if request.method == "POST":
-        # CREATE RECORD
-        new_book = Book(
-            title=request.form["title"],
-            author=request.form["author"],
-            rating=request.form["rating"]
-        )
-        db.session.add(new_book)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template("add.html")
+     return render_template("add.html")
 
 
+@app.route("/senddata", methods=['POST'])
+def senddata():
+    
+    global all_books
+    all_books.append({
+        "title": request.form["title"],
+        "author": request.form["author"],
+        "rating": request.form["rating"],
+    })
+    ins=f"INSERT INTO books VALUES(675, '{request.form['title']}', '{request.form['author']}', '{request.form['rating']}')"
+    print(ins)
+
+
+
+    cursor.execute(ins)
+    db.commit()
+
+
+
+    return render_template("index.html",all_books=all_books)
 
 
 if __name__ == "__main__":
